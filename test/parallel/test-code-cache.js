22f2c4860a8237ea4608022cf68fb5d33a71ecde
cjihrig <cjihrig@gmail.com>
2022-03-22 11:14:19 -0400

    Revert "module,repl: support 'node:'-only core modules"
    This reverts commit 3c4ee5267a522a8f16907c4b3726dd4afb29d4f6.
    
    'node:'-only core modules may not be a great idea.

--- test/parallel/test-code-cache.js
+++ test/parallel/test-code-cache.js
@@ -16,7 +16,7 @@ const {
 } = internalBinding('native_module');
 
 for (const key of canBeRequired) {
-  require(`node:${key}`);
+  require(key);
 }
 
 // The computation has to be delayed until we have done loading modules
